698e05fd87aa658717e809b203e00a2a12f244fd
apoorva-mk <apoorva.mk99@gmail.com>
2019-05-24 16:35:26 +1200

    dwt

--- Matlab/Packages/BLS-GSM/denoising_subprograms/paper2.m
+++ Matlab/Packages/BLS-GSM/denoising_subprograms/paper2.m
@@ -15,5 +15,35 @@ im = im0 + sig*noise;
 figure(2)
 rang = showIm(im,'auto');title('Noisy Image');
 
+[l,h]= size(im0);
+
+%calculating psnr
+peaksnr = psnr(im0,im);
+
+
+%dwt transform
+%first vectorise
+im01 = im0(:);
+
+%now apply wavelet transform
+[cA,cD] = dwt(im01,'sym4');
+
+%cA and cD are the low and high pass filters
+X = idwt(cA,cD,'sym4');
+
+%reshape
+x = reshape(X,[l,h]);
+x = uint8(x);
+figure,imshow(x);
+
+%to view individual components cA and cD, need to upsample
+ca = upsample(cA,2);
+% add 2-1 zeroes intermediately
+% might have to delete some values
+%then reshape and display
+ca = ca(1:l*h);
+y_new = reshape(ca,[l,h]);
+figure,imshow(uint8(y_new));
+
 
 
