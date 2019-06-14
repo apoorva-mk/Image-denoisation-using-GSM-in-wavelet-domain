fb98c4a4f33c4b84b9f7582fc54ff9db8b2a40b9
apoorva-mk <apoorva.mk99@gmail.com>
2019-06-14 14:58:25 +1200

    some changes

--- Matlab/Packages/BLS-GSM/denoising_subprograms/gaussian_dist.m
+++ Matlab/Packages/BLS-GSM/denoising_subprograms/gaussian_dist.m
@@ -1,10 +1,11 @@
 %To find the probability for a given covariance matrix and zero mean)
 
 function prob_density = gaussian_dist( x, covariance_matrix, neighbourhood_size)
-% inv_cov = covariance_matrix\eye(size(covariance_matrix));
-inv_cov = covariance_matrix;
+inv_cov = covariance_matrix\eye(size(covariance_matrix));
+%inv_cov = covariance_matrix;
 num = exp (x' * inv_cov * x * (-0.5)) ;
 denom =  ( (2*(pi))^(neighbourhood_size/2) * (det(covariance_matrix)^(0.5)) );
 prob_density = diag(num)/denom;
 
 
+    
