693d6581a4b9ce0ead8699d9c25c6a19b720eaf6
Shubham Kumar Jha <skjha832@gmail.com>
2019-06-24 13:40:00 +0530

    Changes in gaussian_dist to make it faster

--- Matlab/Packages/BLS-GSM/denoising_subprograms/gaussian_dist.m
+++ Matlab/Packages/BLS-GSM/denoising_subprograms/gaussian_dist.m
@@ -3,9 +3,10 @@
 function prob_density = gaussian_dist( x, covariance_matrix, neighbourhood_size)
 inv_cov = covariance_matrix\eye(size(covariance_matrix));
 %inv_cov = covariance_matrix;
-num = exp (x' * inv_cov * x * (-0.5)) ;
+num = exp ((x' * inv_cov).* x' * (-0.5)) ;
+num = sum(num,2);
 denom =  ( (2*(pi))^(neighbourhood_size/2) * (det(covariance_matrix)^(0.5)) );
-prob_density = diag(num)/denom;
+prob_density = num/denom;
 
 
     
